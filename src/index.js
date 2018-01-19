59eec28d67e92b1302afc2174720c23d0943379b
an.ptitsyn <an.ptitsyn@jet.su>
2018-01-19 15:23:57 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+class HelloWorld {
+  message = 'Hello World';
+  print = () => this.message;
+}
+
+const hello = new HelloWorld();
+// eslint-disable-next-line no-console
+console.log(`%c ${hello.print()}`, 'color: green; font-size:48px; weight: bold');
